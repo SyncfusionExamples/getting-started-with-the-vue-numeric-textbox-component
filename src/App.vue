d4511f5aa1c0b05df1ce4788541e46501b7c87d4
NilubaJashanaS <134999786+NilubaJashanaS@users.noreply.github.com>
2023-12-21 20:47:26 +0530

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script>
+import { NumericTextBoxComponent } from '@syncfusion/ej2-vue-inputs';
+
+export default {
+  components: {
+    'ejs-numerictextbox' : NumericTextBoxComponent
+  }
+}
+
+</script>
+
+<template>
+<div class="numerictextbox">
+ <ejs-numerictextbox width="200px" :value="10" :max="100" :min="1" :step="5"
+  format="000.00" :decimals="2" :validateDecimalOnType="true"
+  :showSpinButton="false" >
+ </ejs-numerictextbox>
+</div>
+</template>
+
+<style>
+  @import "../node_modules/@syncfusion/ej2-base/styles/fluent.css";
+  @import "../node_modules/@syncfusion/ej2-vue-inputs/styles/fluent.css";
+
+  .numerictextbox {
+    margin-bottom: 150px;
+  }
+
+</style>
